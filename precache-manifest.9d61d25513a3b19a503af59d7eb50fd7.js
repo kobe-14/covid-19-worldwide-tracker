b7e75cf9ccc237f8d409b68a7da4b524ac2c854c
Harish <harish@advancedstructures.in>
2020-08-04 13:14:48 +0530

    Updates

---
+++ precache-manifest.9d61d25513a3b19a503af59d7eb50fd7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "909eda455953f9a1e8e202edea00ccb7",
+    "url": "/covid-19-worldwide-tracker/index.html"
+  },
+  {
+    "revision": "31bdf83046be8fce1463",
+    "url": "/covid-19-worldwide-tracker/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "785c517c8c2feab7f194",
+    "url": "/covid-19-worldwide-tracker/static/css/main.bb0bcfbe.chunk.css"
+  },
+  {
+    "revision": "31bdf83046be8fce1463",
+    "url": "/covid-19-worldwide-tracker/static/js/2.03af41ce.chunk.js"
+  },
+  {
+    "revision": "da75538c52411804769b514107a743de",
+    "url": "/covid-19-worldwide-tracker/static/js/2.03af41ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "785c517c8c2feab7f194",
+    "url": "/covid-19-worldwide-tracker/static/js/main.ef618f8b.chunk.js"
+  },
+  {
+    "revision": "ab427faaaa2109eadb10",
+    "url": "/covid-19-worldwide-tracker/static/js/runtime-main.9c176ff0.js"
+  }
+]);
